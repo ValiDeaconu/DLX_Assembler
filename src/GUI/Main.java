cb9323e71d2918e9d3c6e0a48f699134039d0a55
farcasalex <alex14tkd@gmail.com>
2019-10-22 21:56:33 +0300

    GUI Update

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("View/MainPage.fxml"));
+        primaryStage.setTitle("DLX Assembler");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
