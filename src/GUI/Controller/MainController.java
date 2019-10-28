baa5c31e0216285fd25c3e29200b9bac64e15282
farcasalex <alex14tkd@gmail.com>
2019-10-28 18:01:58 +0200

    Fixed output file type display bug

--- src/GUI/Controller/MainController.java
+++ src/GUI/Controller/MainController.java
@@ -1,21 +1,23 @@
 package GUI.Controller;
 
-import GUI.Main;
 import Util.ApplicationSettings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TextArea;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class MainController {
+public class MainController implements Initializable {
 
     @FXML
     private MenuItem openFile;
@@ -55,6 +57,12 @@ public class MainController {
 
     @FXML
     private Button build;
+
+    @FXML
+    private Tab watchesTab;
+
+    @FXML
+    private Tab outputTab;
 
     private ApplicationSettings applicationSettings = new ApplicationSettings();
     private Stage primaryStage;
@@ -129,5 +137,10 @@ public class MainController {
 
     public void setPrimaryStage(Stage primaryStage) {
         this.primaryStage = primaryStage;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        outputTab.setText("output" + applicationSettings.getOutputFileExtension());
     }
 }
