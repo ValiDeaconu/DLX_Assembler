b724b7f7318c3ab94e72790b726c53d780929b6a
Vali <valideaconu98@gmail.com>
2019-10-29 14:07:14 +0200

    Added Instruction Types for FD2 and DF2.

--- src/DataAccess/InstructionType.java
+++ src/DataAccess/InstructionType.java
@@ -55,7 +55,11 @@ public enum InstructionType {
     /** Format: 1 integer register + 1 float register */
     IF2,
     /** Format: 1 float register + 1 integer register */
-    FI2;
+    FI2,
+    /** Format: 1 float register + 1 double register */
+    FD2,
+    /** Format: 1 double register + 1 float register */
+    DF2;
 
     private static Map<InstructionType, List<OperandType>> InstructionTypeToOperands;
 
@@ -70,10 +74,7 @@ public enum InstructionType {
         List<OperandType> operandsType = getOperandsList(type);
 
         if (operandsType == null) {
-            if (instruction.getParametersNumber() == 0)
-                return true;
-            else
-                return false;
+            return instruction.getParametersNumber() == 0;
         }
 
         if (operandsType.size() != instruction.getParametersNumber()) {
@@ -126,8 +127,10 @@ public enum InstructionType {
                     break;
                 }
                 case NUMERIC: {
-                    if (!(operand instanceof UnsignedIntegerOperand || operand instanceof IntegerOperand ||
-                            operand instanceof FloatingOperand || operand instanceof DoubleOperand))
+                    if (!(operand instanceof UnsignedIntegerOperand ||
+                            operand instanceof IntegerOperand ||
+                            operand instanceof FloatingOperand ||
+                            operand instanceof DoubleOperand))
                         return false;
                     break;
                 }
@@ -143,102 +146,32 @@ public enum InstructionType {
         Map<InstructionType, List<OperandType>> map = new HashMap<>();
 
         map.put(InstructionType.UNIMP, null);
-        map.put(InstructionType.REGLAB, Arrays.asList(new OperandType[]{
-                OperandType.INTEGER_REGISTER,
-                OperandType.LABEL
-        }));
-        map.put(InstructionType.REG1IMM, Arrays.asList(new OperandType[]{
-                OperandType.INTEGER_REGISTER,
-                OperandType.NUMERIC
-        }));
-        map.put(InstructionType.REG2IMM, Arrays.asList(new OperandType[]{
-                OperandType.INTEGER_REGISTER,
-                OperandType.INTEGER_REGISTER,
-                OperandType.NUMERIC
-        }));
-        map.put(InstructionType.REG3IMM, Arrays.asList(new OperandType[]{
-                OperandType.INTEGER_REGISTER,
-                OperandType.INTEGER_REGISTER,
-                OperandType.INTEGER_REGISTER
-        }));
-        map.put(InstructionType.LEXP16, Arrays.asList(new OperandType[]{
-                OperandType.LABEL
-        }));
-        map.put(InstructionType.LEXP26, Arrays.asList(new OperandType[] {
-                OperandType.LABEL
-        }));
-        map.put(InstructionType.IREG1, Arrays.asList(new OperandType[]{
-                OperandType.INTEGER_REGISTER,
-        }));
-        map.put(InstructionType.LOADI, Arrays.asList(new OperandType[]{
-                OperandType.INTEGER_REGISTER,
-                OperandType.LABEL,
-                OperandType.INTEGER_REGISTER
-        }));
-        map.put(InstructionType.LOADF, Arrays.asList(new OperandType[]{
-                OperandType.FLOAT_REGISTER,
-                OperandType.LABEL,
-                OperandType.FLOAT_REGISTER
-        }));
-        map.put(InstructionType.LOADD, Arrays.asList(new OperandType[]{
-                OperandType.DOUBLE_REGISTER,
-                OperandType.LABEL,
-                OperandType.DOUBLE_REGISTER
-        }));
-        map.put(InstructionType.STRI, Arrays.asList(new OperandType[]{
-                OperandType.LABEL,
-                OperandType.INTEGER_REGISTER,
-                OperandType.INTEGER_REGISTER
-        }));
-        map.put(InstructionType.STRF, Arrays.asList(new OperandType[]{
-                OperandType.LABEL,
-                OperandType.FLOAT_REGISTER,
-                OperandType.FLOAT_REGISTER
-        }));
-        map.put(InstructionType.STRD, Arrays.asList(new OperandType[]{
-                OperandType.LABEL,
-                OperandType.DOUBLE_REGISTER,
-                OperandType.DOUBLE_REGISTER
-        }));
-        map.put(InstructionType.IMM1, Arrays.asList(new OperandType[]{
-                OperandType.NUMERIC
-        }));
+        map.put(InstructionType.REGLAB, Arrays.asList(OperandType.INTEGER_REGISTER, OperandType.LABEL));
+        map.put(InstructionType.REG1IMM, Arrays.asList(OperandType.INTEGER_REGISTER, OperandType.NUMERIC));
+        map.put(InstructionType.REG2IMM, Arrays.asList(OperandType.INTEGER_REGISTER, OperandType.INTEGER_REGISTER, OperandType.NUMERIC));
+        map.put(InstructionType.REG3IMM, Arrays.asList(OperandType.INTEGER_REGISTER, OperandType.INTEGER_REGISTER, OperandType.INTEGER_REGISTER));
+        map.put(InstructionType.LEXP16, Collections.singletonList(OperandType.LABEL));
+        map.put(InstructionType.LEXP26, Collections.singletonList(OperandType.LABEL));
+        map.put(InstructionType.IREG1, Collections.singletonList(OperandType.INTEGER_REGISTER));
+        map.put(InstructionType.LOADI, Arrays.asList(OperandType.INTEGER_REGISTER, OperandType.LABEL, OperandType.INTEGER_REGISTER));
+        map.put(InstructionType.LOADF, Arrays.asList(OperandType.FLOAT_REGISTER, OperandType.LABEL, OperandType.FLOAT_REGISTER));
+        map.put(InstructionType.LOADD, Arrays.asList(OperandType.DOUBLE_REGISTER, OperandType.LABEL, OperandType.DOUBLE_REGISTER));
+        map.put(InstructionType.STRI, Arrays.asList(OperandType.LABEL, OperandType.INTEGER_REGISTER, OperandType.INTEGER_REGISTER));
+        map.put(InstructionType.STRF, Arrays.asList(OperandType.LABEL, OperandType.FLOAT_REGISTER, OperandType.FLOAT_REGISTER));
+        map.put(InstructionType.STRD, Arrays.asList(OperandType.LABEL, OperandType.DOUBLE_REGISTER, OperandType.DOUBLE_REGISTER));
+        map.put(InstructionType.IMM1, Collections.singletonList(OperandType.NUMERIC));
         map.put(InstructionType.PSEUDO, null);
         map.put(InstructionType.NONEOP, null);
-        map.put(InstructionType.FREG2a, Arrays.asList(new OperandType[]{
-                OperandType.FLOAT_REGISTER,
-                OperandType.FLOAT_REGISTER
-        }));
-        map.put(InstructionType.DREG2a, Arrays.asList(new OperandType[]{
-                OperandType.DOUBLE_REGISTER,
-                OperandType.DOUBLE_REGISTER
-        }));
-        map.put(InstructionType.FREG2b, Arrays.asList(new OperandType[]{
-                OperandType.FLOAT_REGISTER,
-                OperandType.FLOAT_REGISTER
-        }));
-        map.put(InstructionType.DREG2b, Arrays.asList(new OperandType[]{
-                OperandType.DOUBLE_REGISTER,
-                OperandType.DOUBLE_REGISTER
-        }));
-        map.put(InstructionType.FREG3, Arrays.asList(new OperandType[]{
-                OperandType.FLOAT_REGISTER,
-                OperandType.FLOAT_REGISTER,
-                OperandType.FLOAT_REGISTER
-        }));
-        map.put(InstructionType.DREG3, Arrays.asList(new OperandType[]{
-                OperandType.DOUBLE_REGISTER,
-                OperandType.DOUBLE_REGISTER,
-                OperandType.DOUBLE_REGISTER
-        }));
-        map.put(InstructionType.IF2, Arrays.asList(new OperandType[]{
-                OperandType.INTEGER_REGISTER,
-                OperandType.FLOAT_REGISTER
-        }));
-        map.put(InstructionType.FI2, Arrays.asList(new OperandType[]{
-                OperandType.FLOAT_REGISTER,
-                OperandType.INTEGER_REGISTER
-        }));
+        map.put(InstructionType.FREG2a, Arrays.asList(OperandType.FLOAT_REGISTER, OperandType.FLOAT_REGISTER));
+        map.put(InstructionType.DREG2a, Arrays.asList(OperandType.DOUBLE_REGISTER, OperandType.DOUBLE_REGISTER));
+        map.put(InstructionType.FREG2b, Arrays.asList(OperandType.FLOAT_REGISTER, OperandType.FLOAT_REGISTER));
+        map.put(InstructionType.DREG2b, Arrays.asList(OperandType.DOUBLE_REGISTER, OperandType.DOUBLE_REGISTER));
+        map.put(InstructionType.FREG3, Arrays.asList(OperandType.FLOAT_REGISTER, OperandType.FLOAT_REGISTER, OperandType.FLOAT_REGISTER));
+        map.put(InstructionType.DREG3, Arrays.asList(OperandType.DOUBLE_REGISTER, OperandType.DOUBLE_REGISTER, OperandType.DOUBLE_REGISTER));
+        map.put(InstructionType.IF2, Arrays.asList(OperandType.INTEGER_REGISTER, OperandType.FLOAT_REGISTER));
+        map.put(InstructionType.FI2, Arrays.asList(OperandType.FLOAT_REGISTER, OperandType.INTEGER_REGISTER));
+        map.put(InstructionType.FD2, Arrays.asList(OperandType.FLOAT_REGISTER, OperandType.DOUBLE_REGISTER));
+        map.put(InstructionType.DF2, Arrays.asList(OperandType.DOUBLE_REGISTER, OperandType.FLOAT_REGISTER));
 
         InstructionTypeToOperands = map;
     }
